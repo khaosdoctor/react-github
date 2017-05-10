4d9a4233ead749659fbe5f5f23e91181df23e7ea
Lucas Santos <lhs.santoss@gmail.com>
2017-05-09 23:20:39 -0300

    Rendering component on index

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var GitHub = require('./components/gitHub');
 
 //O RDOM é uma lib que vai renderizar um componente (ou um JSX como abaixo)
 //Em um elemento cujo ID será passado no segundo argumento
-ReactDOM.render(<h1>Teste</h1>, document.getElementById('app'));
+ReactDOM.render(<GitHub />, document.getElementById('app'));
